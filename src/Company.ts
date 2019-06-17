6a94e920d0b4a04b35a89fa1c8e124d9c84de3f9
unachoza <unachoza@gmail.com>
2019-06-17 12:44:41 -0400

    added google maps api

--- src/Company.ts
+++ src/Company.ts
@@ -1,14 +1,15 @@
 import faker from "faker";
 
-export class User {
-  name: string;
+export class Company {
+  companyName: string;
+  catchPhras: string;
   location: {
     lat: number;
     lng: number;
   };
-
   constructor() {
-    this.name = faker.name.firstName();
+    this.companyName = faker.company.companyName();
+    this.catchPhras = faker.company.catchPhrase();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('I work')
+import { User } from "./User";
+import {Company} from './Company'
+
+
+const user = new User()
+const company = new Company()
+
+console.log(user, company)
+
+google 
+
